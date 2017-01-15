9516a2203208a070d47c1bc1444efb4e45edb780
Ruaghain <ruaghain_dev@hotmail.com>
2017-01-15 14:42:39 +0000

    Added in a new sign up component.

---
+++ assets/app/signup/signup.component.spec.ts
@@ -0,0 +1,9 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { DebugElement } from "@angular/core";
+
+import { SignupComponent } from "./signup.component";
+
+describe("SignupComponent", () => {
+
+});
