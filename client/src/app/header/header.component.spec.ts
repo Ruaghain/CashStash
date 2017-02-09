5471031a32bce604211b955d43bc37114f0e11d1
Ruaghain <ruaghain_dev@hotmail.com>
2017-02-09 21:06:20 +0000

    Added in a few new tests for the headerComponent.

--- client/src/app/header/header.component.spec.ts
+++ client/src/app/header/header.component.spec.ts
@@ -22,20 +22,12 @@ describe("HeaderComponent", () => {
     });
   }));
 
-  it('should display a form to register', () => {
-    let navbar = fixture.debugElement.query(By.css('navbar'));
-    expect(navbar).toBeDefined();
+  it('should be an instance of HeaderComponent', () => {
+    expect(fixture.componentInstance instanceof HeaderComponent).toBe(true, 'should be of type HeaderComponent');
+  });
+
+  it('should have a brand name', () => {
+    let brandName = fixture.debugElement.query(By.css('.navbar-brand'));
+    expect(brandName.nativeElement.textContent).toEqual('CashStash');
   });
 });
-
-// import { TestBed } from '@angular/core/testing';
-// import { AppComponent } from './app.component';
-// describe('App', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({ declarations: [AppComponent]});
-//   });
-//   it ('should work', () => {
-//     let fixture = TestBed.createComponent(AppComponent);
-//     expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
-//   });
-// });
