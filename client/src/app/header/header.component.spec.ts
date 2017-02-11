a386b1d44a1020d6288d330379f8f1ce3c49a7b3
Ruaghain <ruaghain_dev@hotmail.com>
2017-02-11 14:29:29 +0000

    Added in the ability to display the logged in user name, and an admin section along with a logout option in the dropdown for the logged in user.

--- client/src/app/header/header.component.spec.ts
+++ client/src/app/header/header.component.spec.ts
@@ -38,19 +38,34 @@ describe("HeaderComponent", () => {
     expect(element.textContent).toEqual('CashStash');
   });
 
-  it('should display the "logout" button if the user is logged in', () => {
-    component.isUserLoggedIn = true;
-    fixture.detectChanges();
-    debugElement = fixture.nativeElement.querySelectorAll('ol>li');
-    let logoutItem = Array.prototype.slice.call(debugElement);
-    expect(logoutItem.length).toBe(1, 'There should only be a logout button displayed for a logged in user.');
-  });
-
   it('should display the "Sign In" and "Sign Up" buttons if the user is not logged in', () => {
-    component.isUserLoggedIn = false;
+    component.setUserLoggedIn(false);
     fixture.detectChanges();
     debugElement = fixture.nativeElement.querySelectorAll('ol>li');
     let logoutItem = Array.prototype.slice.call(debugElement);
     expect(logoutItem.length).toBe(2, 'There should be a login and signup button displayed for a user who is no logged in.');
   });
+
+  describe('user logged in', () => {
+
+    beforeEach(() => {
+      component.setUserLoggedIn(true);
+      component.setUserFullName('One User');
+      fixture.detectChanges();
+    });
+
+    it('should display the users full name', () => {
+      element = fixture.nativeElement.querySelector('ol>li>a');
+      expect(element.innerText).toBe('One User', 'There should only be a logout button displayed for a logged in user.');
+    });
+
+    it('should display two options in the dropdown', () => {
+      element = fixture.nativeElement.querySelectorAll('ol>li>ul>li>a');
+      let dropdownItems = Array.prototype.slice.call(element);
+      expect(dropdownItems.length).toBe(2, 'There should only be two options in the logged in dropdown.');
+      expect(dropdownItems[0].textContent).toBe('Administration', 'The first element should be "Administration"');
+      expect(dropdownItems[1].textContent).toBe('Logout', 'The last element should be "Logout"');
+    })
+  });
+
 });
--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'cash-header',
@@ -6,11 +6,30 @@ import { Component } from "@angular/core";
   templateUrl: 'header.component.html'
 })
 
-export class HeaderComponent {
+export class HeaderComponent extends OnInit {
 
-  isUserLoggedIn: boolean;
+  private headerUserLoggedIn: boolean;
+  private headerUserFullName: string;
 
-  userLoggedIn = () => {
-    return this.isUserLoggedIn;
+  ngOnInit(): void {
+    this.setUserLoggedIn(true);
+    this.setUserFullName("Rowan Massey");
+  }
+
+  //Will get the following from the authorisation service when it's created.
+  setUserLoggedIn = (userLoggedIn: boolean) => {
+    this.headerUserLoggedIn = userLoggedIn;
+  };
+
+  setUserFullName = (userFullName: string) => {
+    this.headerUserFullName = userFullName;
+  };
+
+  isUserLoggedIn = () => {
+    return this.headerUserLoggedIn;
+  };
+
+  userFullName = () => {
+    return this.headerUserFullName;
   }
 }
