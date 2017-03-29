b5690626daea41fa4914bd765424d317dd2f3d34
Ruaghain <ruaghain_dev@hotmail.com>
2017-03-29 22:21:23 +0100

    1. Installed `Helmet` and commented out the other setHeader methods as this should be taken care of by helmet. 2. Added in npm scripts to the `package.json` to start and test the application. 3. Added in Log4js as the logger. Seems to work a lot better. 4. Got rid of the mongoose promise issue. Also installed v8-profiler to see how it works.
    Fixes #19

--- server/api/user/user.route.js
+++ server/api/user/user.route.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var logger = require('../../utils/logging').log;
 var router = express.Router();
 
 var User = require('./user.model');
 
 router.post('/', function (req, res) {
+  logger.debug('Saving new user "%s"', req.body.userName);
   var user = new User({
     userName: req.body.userName,
     firstName: req.body.firstName,
@@ -13,6 +15,7 @@ router.post('/', function (req, res) {
   });
   user.save(function (err, result) {
     if (err) {
+      logger.error('There was an error saving user "%s"', user.userName);
       //Need to return here because you want it to return immediately, and not execute
       //the last code - below this return statement.
       return res.status(500).json({
@@ -20,12 +23,13 @@ router.post('/', function (req, res) {
         error: err
       })
     }
+    logger.debug('Successfully created the new user.');
     //Don't need return here as it's the last statement.
     res.status(201).json({
       message: 'User created',
       obj: result
     })
-  })
+  }).then()
 });
 
 module.exports = router;
--- server/config/express.js
+++ server/config/express.js
@@ -2,11 +2,11 @@ var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./environment');
 
+var helmet = require('helmet');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-
+var logger = require('./../utils/logging');
 var bodyParser = require('body-parser');
 
 module.exports = function (app) {
@@ -15,18 +15,22 @@ module.exports = function (app) {
   // view engine setup
   app.set('views', config.root + '/server/views');
   app.set('view engine', 'hbs');
-
+  app.use(logger.log4js.connectLogger(logger.log, {
+    level: config.logging,
+    format: ':method :url',
+    nolog: '\\.gif|\\.jpg$|\\.ico$'
+  }));
+  app.use(helmet());
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-  app.use(logger('dev'));
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(cookieParser());
   app.use(express.static(path.join(config.root, 'dist')));
 
-  app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-    next();
-  });
+  // app.use(function (req, res, next) {
+  //   res.setHeader('Access-Control-Allow-Origin', '*');
+  //   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  //   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  //   next();
+  // });
 };
